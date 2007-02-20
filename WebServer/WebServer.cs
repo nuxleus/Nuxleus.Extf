fba13dfa0a0fadcd5809d2cf88a83d23ce013a71
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-02-20 01:16:39 +0000

    For starting, stopping, and in other ways, controlling the webserver instance.
    git-svn-id: http://extf.googlecode.com/svn/branches/xameleon-extension-work@38 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ WebServer/WebServer.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Net;
+using System.Web;
+using Mono.WebServer;
+using System.IO;
+
+namespace Xameleon {
+
+  public class WebServer {
+
+    public WebServer () { }
+
+    private string App = "/:.";
+    private int Port = 18080;
+    private string Path = "/";
+    private string RootDirectory = ".\\Web\\Public";
+    private bool Verbose = true;
+    private XSPWebSource websource;
+    private ApplicationServer WebAppServer;
+
+    public void Start () {
+      Environment.CurrentDirectory = RootDirectory;
+      this.websource = new XSPWebSource(IPAddress.Any, this.Port);
+      this.WebAppServer = new ApplicationServer(websource);
+      this.WebAppServer.AddApplication(this.App, this.Port, this.Path, Directory.GetCurrentDirectory());
+      this.WebAppServer.Verbose = this.Verbose;
+      this.WebAppServer.Start(false);
+    }
+
+    public void Stop () {
+      this.WebAppServer.UnloadAll();
+      this.WebAppServer.Stop();
+    }
+
+    public void SetApp (string app) {
+      this.App = app;
+    }
+
+    public void SetVerbose (bool verbose) {
+      this.Verbose = verbose;
+    }
+
+    public void SetPort (int port) {
+      this.Port = port;
+    }
+
+    public void SetPath (string path) {
+      this.Path = path;
+    }
+
+    public void SetRoot (string root) {
+      this.RootDirectory = root;
+    }
+
+    public void SetSystemRoot (string sys) {
+      Environment.CurrentDirectory = sys;
+    }
+
+    public void GetCurDir () {
+      System.Console.WriteLine(Directory.GetCurrentDirectory());
+    }
+
+    public void AddApp (String app, String path) {
+      this.WebAppServer.AddApplication(app, this.Port, path, Directory.GetCurrentDirectory());
+    }
+  }
+}
