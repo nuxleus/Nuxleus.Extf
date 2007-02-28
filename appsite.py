a50b476eadaf216ebe90cdecd8ddd165cc1d55fd
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-02-28 20:50:53 +0000

    git-svn-id: http://extf.googlecode.com/svn/trunk/WebApp@100 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ appsite.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+import selector
+from amplee.handler.store.wsgi import Service, Store
+
+# Local imports
+from core.atompub import setup_store
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Our main WSGI application is the selector middleware
+# which will dispatch to the amplee WSGI applications
+# based on the request URI
+s = selector.Selector()
+
+def create_store(dispatcher):
+    print "Creating APP store"
+    service, conf = setup_store()
+    workspace = service.workspaces[0]
+    collections = service.get_collections()
+    
+    print "Creating the service WSGI application"
+    service = Service(service)
+    # Uri to which the app:service document is reachable
+    dispatcher.add('/service', GET=service.get_service)
+    
+    print "Creating the collection WSGI application"
+    music_store = Store(workspace.get_collection('music'), strict=True)
+    dispatcher.add('[/]', POST=music_store.create_member,
+          GET=music_store.get_collection, HEAD=music_store.head_collection)
+    dispatcher.add('/{rid:any}', GET=music_store.get_member,
+          PUT=music_store.update_member,
+          DELETE=music_store.delete_member, HEAD=music_store.head_member)
+    print "All good!"
+
+create_store(s)
+
+app = s
---
+++ core/atompub.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+from amplee.loader import loader, Config
+
+#########################################################################
+# A couple of global but handy variables
+#########################################################################
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+
+def setup_store():
+    """
+    Loads into memory our APP store from the configuration file
+    """
+    service, conf = loader(os.path.join(base_dir, 'appstore.conf'),
+                           encoding='ISO-8859-1', base_path=base_dir)
+
+    return service, conf
+
+#########################################################################
+# Utils
+#########################################################################
+def atom_id_generator(collection, seed, slug, *args, **kwargs):
+    """
+    Through this function you can provide the precise atom:id value
+    you want for your member.
+    
+    For the purpose of this example this function will be fairly dumb
+    and will simply construct the absolute URI to the media member
+    resource.
+    """
+    title = seed.get_child('title', seed.xml_ns)
+    if title:
+        title = title.xml_text
+    else:
+        title = slug
+    id = unicode('%s/%s%s' % (collection.xml_base or '',
+                              collection.base_uri, title.replace(' ', '-')))
+    # When xml_base is not set the previous call leaves a leading '/'
+    # we just get rid of it here
+    return id.lstrip('/')
+
+
+def resource_storage_name_generator(base_uri, slug, title):
+    """
+    This method returns the value that will be used to identify
+    the resource within your storage.
+
+    So if your Slug header looks like 'my great blog post' this
+    will return 'my-great-blog-post' which will be used to
+    store the resource.
+
+    If a slug is not provided we try with the title of the
+    member resource if any.
+    """
+    if slug:
+        return unicode('%s' % slug.replace(' ', '-'))
+    if title:
+        return unicode('%s' % title.replace(' ', '-'))
+    raise ValueError, 'Missing slug or title to generate the resource name'
