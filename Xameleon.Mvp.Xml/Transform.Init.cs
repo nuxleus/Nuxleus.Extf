fa8234189a54567929b6233e82a8e008db16d3db
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-03-06 20:52:31 +0000

    git-svn-id: http://extf.googlecode.com/svn/trunk@168 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon.Mvp.Xml/Transform.Init.cs
+++ Xameleon.Mvp.Xml/Transform.Init.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Xml;
 using System.IO;
 using Saxon.Api;
-using Xameleon.Saxon.Properties;
+using Xameleon.Properties;
 using System.Net;
 
 namespace Xameleon.Saxon {
