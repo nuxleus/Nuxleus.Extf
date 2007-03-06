3a3f5f60924d050ac1a1202bb25c62c3728c193c
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-03-06 14:44:13 +0000

    git-svn-id: http://extf.googlecode.com/svn/trunk@155 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- VendorExtensions/Saxonica/Saxon.Ext/Extensions/Extensions.cs
+++ VendorExtensions/Saxonica/Saxon.Ext/Extensions/Extensions.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Query;
-using System.Xml.XLinq;
-using System.Data.DLinq;
-using System.Windows.Forms;
-using Saxon.Api;
-using Saxon.Ext;
-using System.Xml;
 
 
-namespace Saxon.Ext {
+
+namespace Xameleon.Ext {
 
 }
 
