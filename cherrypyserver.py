0f81071bd9deceb8995e0a20ab5c0fa75d87f2f9
sylvain.hellegouarch <sylvain.hellegouarch@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-03-07 21:59:35 +0000

    allow server to be ran from the console
    git-svn-id: http://extf.googlecode.com/svn/trunk/WebApp@170 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- cherrypyserver.py
+++ cherrypyserver.py
@@ -7,28 +7,22 @@ cur_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin')
 sys.path.append(cur_dir)
 clr.AddReferenceToFile('Xameleon.dll')
 
-## from cherrypy import wsgiserver
-## wsgi_apps = [('', app)]
+import os
+import cherrypy
+from appsite import app
 
-## server = wsgiserver.CherryPyWSGIServer(('localhost', 9999), wsgi_apps,
-##                                        server_name='localhost')
-
-if __name__ == '__main__':
-##     try:
-##         print "Serving HTTP on http://localhost:9999"
-##         server.start()
-##     except KeyboardInterrupt:
-##         server.stop()
-
-    import os
-    import cherrypy
-
+def run(blocking=False):
     cur_dir = os.getcwd()
     cherrypy.config.update({'checker.on': False,
                             'engine.autoreload_on': False,
                             'server.socket_port': 9999})
 
-    from appsite import app
     cherrypy.tree.graft(app)
     cherrypy.server.quickstart()
-    cherrypy.engine.start()
+    cherrypy.engine.start(blocking=blocking)
+
+def shutdown():
+    cherrypy.engine.stop()
+
+if __name__ == '__main__':
+    run(blocking=True)
