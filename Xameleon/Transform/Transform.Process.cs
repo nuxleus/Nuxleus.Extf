9ab6d0bc783a7068c2aaef7991346eb2b418eadd
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-02 23:14:04 +0000

    added header exteion function
    git-svn-id: http://extf.googlecode.com/svn/trunk@457 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Transform/Transform.Process.cs
+++ Xameleon/Transform/Transform.Process.cs
@@ -87,7 +87,7 @@ namespace Xameleon
                     
                     // temporary hack
                     transformer.SetParameter(new QName("", "", "request.ip"), new XdmAtomicValue(request.UserHostAddress));
-                    transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response.ContentType)));
+                    transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response)));
                     // end temporary hack
                     transformer.InputXmlResolver = this._Resolver;
                     transformer.InitialContextNode = node;
