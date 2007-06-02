3547c7f3316f01cb809ff9eb7eb9eb68ca93f7b5
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-02 22:07:32 +0000

    updated with proper code base
    git-svn-id: http://extf.googlecode.com/svn/trunk@453 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Transform/Transform.Process.cs
+++ Xameleon/Transform/Transform.Process.cs
@@ -84,9 +84,10 @@ namespace Xameleon
                             transformer.SetParameter(new QName("", "", "cookie_" + local), new XdmAtomicValue(request.Cookies[local].Value));
                         }
                     }
+
                     // temporary hack
                     transformer.SetParameter(new QName("", "", "request.ip"), new XdmAtomicValue(request.UserHostAddress));
-                    transformer.SetParameter(new QName("", "", "response"), new XdmValue(XdmAtomicValue.wrapExternalObject(response.Headers)));
+                    transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response)));
                     // end temporary hack
                     transformer.InputXmlResolver = this._Resolver;
                     transformer.InitialContextNode = node;
