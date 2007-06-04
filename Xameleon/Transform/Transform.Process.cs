2f43743f0f8e5a4efecad8c2343abf23b5eb5828
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-04 03:58:26 +0000

    added cast of SequenceEnumerator for the cookie collection
    git-svn-id: http://extf.googlecode.com/svn/trunk@468 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Transform/Transform.Process.cs
+++ Xameleon/Transform/Transform.Process.cs
@@ -6,6 +6,7 @@ using Saxon.Api;
 using System.IO;
 using System.Web;
 using System.Collections;
+using System.Collections.Specialized;
 
 namespace Xameleon
 {
@@ -91,7 +92,7 @@ namespace Xameleon
                     transformer.SetParameter(new QName("", "", "timestamp"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Timestamp)));
                     transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response)));
                     transformer.SetParameter(new QName("", "", "request"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(request)));
-                    transformer.SetParameter(new QName("", "", "cookies"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(collection)));
+                    transformer.SetParameter(new QName("", "", "cookies"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject((SequenceEnumerator)collection)));
                     transformer.SetParameter(new QName("", "", "form"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(request.Form)));
                     transformer.SetParameter(new QName("", "", "querystring"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(request.QueryString)));
 
