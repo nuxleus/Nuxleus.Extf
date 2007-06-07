d0241671cdd004dc30270db49ac394ed23deb0e3
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-07 07:01:32 +0000

    added additional method for setting cookie values
    git-svn-id: http://extf.googlecode.com/svn/trunk@490 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Function/HttpResponseCollection.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace Xameleon.Function {
+
+    public class HttpResponseCollection {
+
+        public static void SetValue(HttpResponse response, string type, string key, string value) {
+            try {
+                switch (type) {
+                    case "cookie":
+                        HttpCookie cookie = new HttpCookie(key, value);
+                        response.Cookies.Set(cookie);
+                        break;
+
+                    default:
+                        break;
+                }
+                
+            } catch (Exception e) {
+                Debug.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
+
+
