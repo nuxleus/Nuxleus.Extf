78f26142f5d0c53a33a5d290a88b545a0734e10b
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-10 22:40:23 +0000

    basic framework used for generating and validating a nonce value for authentication and session validation
    git-svn-id: http://extf.googlecode.com/svn/trunk@505 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Service/Authentication.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using System.Collections;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using Xameleon.Function;
+using Extf.Net.S3;
+using System.Collections.Specialized;
+using System.Web.Configuration;
+
+namespace Xameleon.Service {
+    /// <summary>
+    /// Summary description for Authenticate service
+    /// </summary>
+    [WebService(Namespace = "http://xameleon.org/service")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class Authenticate : WebService {
+
+        static string pubKey = GetSetting("xsltParam_aws-public-key");
+        static string privKey = GetSetting("xsltParam_aws-private-key");
+        AWSAuthConnection conn = new AWSAuthConnection(pubKey, privKey, false);
+
+        [WebMethod(EnableSession = true)]
+        public bool CheckAuthentication(string nonce) {
+            return false;
+        }
+
+        private string GenerateNonce(string sessionKey) {
+            return "foo";
+        }
+
+
+        public static String GetSetting(String keyName) {
+
+            NameValueCollection appSettings = WebConfigurationManager.AppSettings as NameValueCollection;
+
+            IEnumerator appSettingsEnum = appSettings.GetEnumerator();
+
+            try {
+                for (int i = 0; appSettingsEnum.MoveNext(); i++) {
+                    string key = appSettings.AllKeys[i].ToString();
+                    if (key == keyName) {
+                        return appSettings[key];
+                    }
+                }
+            } catch (Exception e) {
+                return e.Message;
+            }
+
+            return null;
+        }
+    }
+}
+
