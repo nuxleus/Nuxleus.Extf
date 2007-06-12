9dd040cc48eb16a762ad4716335c915018a8b00d
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-12 00:13:56 +0000

    reesaped the URI string
    git-svn-id: http://extf.googlecode.com/svn/trunk@511 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Base/S3/QueryStringAuthGenerator.cs
+++ Base/S3/QueryStringAuthGenerator.cs
@@ -158,10 +158,10 @@ namespace Extf.Net.S3 {
         }
 
         public string get(string bucket, string key, SortedList headers) {
-            return generateURL("GET", bucket, HttpUtility.UrlEncode(key), headers);
+            return generateURL("GET", bucket, key, headers);
         }
         public string get(string bucket, string key) {
-            return generateURL("GET", bucket, HttpUtility.UrlEncode(key), null);
+            return generateURL("GET", bucket, key, null);
         }
         public string delete(string bucket, string key, SortedList headers) {
             return generateURL("DELETE", bucket, HttpUtility.UrlEncode(key), headers);
@@ -221,7 +221,7 @@ namespace Extf.Net.S3 {
                 buffer.Append(bucket);
                 if (key != null) {
                     buffer.Append("/");
-                    buffer.Append(HttpUtility.UrlEncode(key));
+                    buffer.Append(key);
                 }
             }
             return buffer.ToString();
