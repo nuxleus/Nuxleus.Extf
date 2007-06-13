d8cf13530b618e301fbf72839293b796709410e6
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-13 11:49:10 +0000

    a bit more cleanup
    git-svn-id: http://extf.googlecode.com/svn/trunk@524 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/SemWeb/Select.cs
+++ Xameleon/SemWeb/Select.cs
@@ -27,20 +27,24 @@ namespace Xameleon.SemWeb {
             reader.BaseUri = uri.OriginalString;
             store.Import(reader);
 
-            builder.AppendLine(@"<service-result>");
-            builder.AppendLine(@"<statement>These are the people in the file:</statement>");
+            builder.AppendLine("<service-result>");
+            builder.AppendLine(@"<statement>
+                               These are the people in the file:
+                               </statement>");
             foreach (Statement s in store.Select(new Statement(null, rdftype, foafPerson))) {
                 foreach (Resource r in store.SelectObjects(s.Subject, foafname))
                     builder.AppendLine(@"<entry>" + r + "</entry>");
             }
 
-            builder.AppendLine(@"<statement>And here's RDF/XML just for some of the file:</statement>");
+            builder.AppendLine(@"<statement>
+                               And here's RDF/XML just for some of the file:
+                               </statement>");
 
             using (RdfWriter w = new RdfXmlWriter(new StringWriter(builder))) {
                 store.Select(new Statement(null, foafname, null), w);
                 store.Select(new Statement(null, foafknows, null), w);
             }
-            builder.AppendLine(@"</service-result>");
+            builder.AppendLine("</service-result>");
             return builder.ToString();
         }
 
