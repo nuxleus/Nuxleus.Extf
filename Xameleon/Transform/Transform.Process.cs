353c1fc1699028eb10f89c4b2e932588ab72170b
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-15 02:20:41 +0000

    added using statement to ensure response stream is closed
    git-svn-id: http://extf.googlecode.com/svn/trunk@530 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Transform/Transform.Process.cs
+++ Xameleon/Transform/Transform.Process.cs
@@ -32,40 +32,43 @@ namespace Xameleon {
             if (!this._IS_INITIALIZED) {
                 this.Init(context);
             }
-            using (Stream input = ((Stream)this._Resolver.GetEntity(absoluteUri, null, typeof(Stream)))) {
-                using (Stream transform = this._TemplateStream) {
-                    DocumentBuilder builder = this._Processor.NewDocumentBuilder();
-                    builder.BaseUri = absoluteUri;
-                    XdmNode node = builder.Build(input);
-                    Serializer destination = new Serializer();
-                    destination.SetOutputWriter(writer);
-                    XsltTransformer transformer = this._Template.Load();
-                    if (this._XsltParams.Count > 0) {
-                        IEnumerator enumerator = this._XsltParams.GetEnumerator();
-                        for (int i = 0; enumerator.MoveNext(); i++) {
-                            string local = this._XsltParams.AllKeys[i].ToString();
-                            transformer.SetParameter(new QName("", "", local), new XdmAtomicValue(this._XsltParams[local]));
+
+            using (writer){
+                using (Stream input = ((Stream)this._Resolver.GetEntity(absoluteUri, null, typeof(Stream)))) {
+                    using (Stream transform = this._TemplateStream) {
+                        DocumentBuilder builder = this._Processor.NewDocumentBuilder();
+                        builder.BaseUri = absoluteUri;
+                        XdmNode node = builder.Build(input);
+                        Serializer destination = new Serializer();
+                        destination.SetOutputWriter(writer);
+                        XsltTransformer transformer = this._Template.Load();
+                        if (this._XsltParams.Count > 0) {
+                            IEnumerator enumerator = this._XsltParams.GetEnumerator();
+                            for (int i = 0; enumerator.MoveNext(); i++) {
+                                string local = this._XsltParams.AllKeys[i].ToString();
+                                transformer.SetParameter(new QName("", "", local), new XdmAtomicValue(this._XsltParams[local]));
+                            }
                         }
-                    }
 
-                    Hashtable results = new Hashtable();;
-                    if (outputS3) {
-                        transformer.ResultDocumentHandler = new S3ResultDocumentHandler(results);
-                    }
-                    transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response)));
-                    transformer.SetParameter(new QName("", "", "request"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(request)));
-                    transformer.SetParameter(new QName("", "", "server"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Server)));
-                    transformer.SetParameter(new QName("", "", "session"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Session)));
-                    transformer.SetParameter(new QName("", "", "timestamp"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Timestamp)));
-                    transformer.InputXmlResolver = this._Resolver;
-                    transformer.InitialContextNode = node;
-                    transformer.Run(destination);
+                        Hashtable results = new Hashtable(); ;
+                        if (outputS3) {
+                            transformer.ResultDocumentHandler = new S3ResultDocumentHandler(results);
+                        }
+                        transformer.SetParameter(new QName("", "", "response"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(response)));
+                        transformer.SetParameter(new QName("", "", "request"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(request)));
+                        transformer.SetParameter(new QName("", "", "server"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Server)));
+                        transformer.SetParameter(new QName("", "", "session"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Session)));
+                        transformer.SetParameter(new QName("", "", "timestamp"), new XdmValue((XdmItem)XdmAtomicValue.wrapExternalObject(context.Timestamp)));
+                        transformer.InputXmlResolver = this._Resolver;
+                        transformer.InitialContextNode = node;
+                        transformer.Run(destination);
 
-                    if (outputS3) {
-                        foreach (DictionaryEntry entry in results) {
-                            string uri = (string)entry.Key;
-                            DomDestination dom = (DomDestination)results[uri];
-                            //TODO: Output results to S3
+                        if (outputS3) {
+                            foreach (DictionaryEntry entry in results) {
+                                string uri = (string)entry.Key;
+                                DomDestination dom = (DomDestination)results[uri];
+                                //TODO: Output results to S3
+                            }
                         }
                     }
                 }
