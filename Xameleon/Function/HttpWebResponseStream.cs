d52776a542fce91d8b3c5dbe4f67c6f5fa388f63
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-18 01:55:24 +0000

    this provides the ability to take a stream from an exteranl web request and return the result
    git-svn-id: http://extf.googlecode.com/svn/trunk@556 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Function/HttpWebResponseStream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Xameleon.Function {
+
+    public static class HttpWebResponseStream {
+
+        public static string GetResponseString(Stream stream) {
+            StringBuilder builder = new StringBuilder();
+            using (StringWriter writer = new StringWriter(builder)) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    builder.Append(reader.ReadToEnd());
+                }
+                return writer.ToString();
+            }
+        }
+    }
+}
