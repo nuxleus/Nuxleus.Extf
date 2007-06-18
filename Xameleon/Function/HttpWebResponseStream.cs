1d1af7897dc00d4eae96afa95edef7e5a43b98b8
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-18 01:58:42 +0000

    more efficient
    git-svn-id: http://extf.googlecode.com/svn/trunk@557 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Function/HttpWebResponseStream.cs
+++ Xameleon/Function/HttpWebResponseStream.cs
@@ -8,12 +8,8 @@ namespace Xameleon.Function {
     public static class HttpWebResponseStream {
 
         public static string GetResponseString(Stream stream) {
-            StringBuilder builder = new StringBuilder();
-            using (StringWriter writer = new StringWriter(builder)) {
-                using (StreamReader reader = new StreamReader(stream)) {
-                    builder.Append(reader.ReadToEnd());
-                }
-                return writer.ToString();
+            using (StreamReader reader = new StreamReader(stream)) {
+                return reader.ReadToEnd();
             }
         }
     }
