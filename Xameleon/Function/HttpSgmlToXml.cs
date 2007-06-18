6da7a9d095f0eb44aa27b8d0ed80737cc69b9741
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-18 13:50:43 +0000

    added a few more method overloads
    git-svn-id: http://extf.googlecode.com/svn/trunk@586 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Function/HttpSgmlToXml.cs
+++ Xameleon/Function/HttpSgmlToXml.cs
@@ -4,30 +4,57 @@ using Sgml;
 using System.Xml;
 using Saxon.Api;
 using net.sf.saxon.om;
+using net.sf.saxon.value;
+using System.IO;
 
 namespace Xameleon.Function {
 
     public static class HttpSgmlToXml {
 
-        public static ValueRepresentation GetDocXml(String uri) {
+        public static Value GetDocXml(String uri) {
             return getDocXml(uri, "/html");
         }
 
-        public static ValueRepresentation GetDocXml(String uri, String path) {
+        public static Value GetDocXml(String uri, String path) {
             return getDocXml(uri, path);
         }
 
-        private static ValueRepresentation getDocXml(String uri, String path) {
-            SgmlReader sr = new SgmlReader();
-            sr.Href = uri;
+        public static String GetDocXml(String uri, String path, bool stripNS) {
+            try {
+                return getXdmNode(uri, path).OuterXml;
+            } catch (Exception e) {
+                throw;
+            }
+        }
 
-            XmlDocument htmlDoc = new XmlDocument();
-            htmlDoc.Load(sr);
+        private static Value getDocXml(String uri, String path) {
+            try {
+                return Value.asValue(getXdmNode(uri, path).Unwrap());
+            } catch (Exception e) {
+                throw;
+            }
+        }
 
-            XmlNode html = htmlDoc.SelectSingleNode(path);
+        private static XdmNode getXdmNode(String uri, String path) {
+            try {
+                SgmlReader sr = new SgmlReader();
+                sr.Href = uri;
 
-            Processor processor = new Processor();
-            return processor.NewDocumentBuilder().Build(html).Unwrap();
+                XmlDocument htmlDoc = new XmlDocument();
+
+                try {
+                    htmlDoc.Load(sr);
+                } catch (Exception e) {
+                    throw;
+                }
+
+                XmlNode html = htmlDoc.SelectSingleNode(path);
+                Processor processor = new Processor();
+                return processor.NewDocumentBuilder().Build(html);
+
+            } catch (Exception e) {
+                throw;
+            }
         }
     }
 }
