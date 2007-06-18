4d2190ee182850b5ab7e8c9d59f77cee8a6af062
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-18 07:42:37 +0000

    added SGMLtoXML functionality
    git-svn-id: http://extf.googlecode.com/svn/trunk@563 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Function/HttpSgmlToXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Saxon.Api;
+using Sgml;
+using System.Xml;
+using Saxon.Api;
+
+namespace Xameleon.Function {
+
+    public static class HttpSgmlToXml {
+
+        public static XdmNode GetDocXml(String uri) {
+            return getDocXml(uri, "/html");
+        }
+
+        public static XdmNode GetDocXml(String uri, String path) {
+            return getDocXml(uri, path);
+        }
+
+        private static XdmNode getDocXml(String uri, String path) {
+            SgmlReader sr = new SgmlReader();
+            sr.Href = uri;
+
+            XmlDocument htmlDoc = new XmlDocument();
+            htmlDoc.Load(sr);
+
+            XmlNode html = htmlDoc.SelectSingleNode(path);
+
+            Processor processor = new Processor();
+            return processor.NewDocumentBuilder().Build(html);
+        }
+    }
+}
