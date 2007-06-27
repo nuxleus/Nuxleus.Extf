a4e72b989b220992021b78ed63922edc273b87fb
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-27 15:38:50 +0000

    and this too
    git-svn-id: http://extf.googlecode.com/svn/trunk@637 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/UrlResolver/S3XmlUrlResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Net;
+
+namespace Xameleon.UrlResolver {
+    public class S3XmlResolver : XmlUrlResolver {
+        public override object GetEntity(Uri absoluteUri, String role, Type ofObjectToReturn) {
+            String uri = absoluteUri.GetComponents(UriComponents.AbsoluteUri, UriFormat.Unescaped);
+            WebResponse response = WebRequest.Create(uri).GetResponse();
+            return (Stream)response.GetResponseStream();
+        }
+    }
+}
