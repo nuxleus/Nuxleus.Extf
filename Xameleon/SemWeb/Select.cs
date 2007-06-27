96a63c9e471041a6fbfd8dc8c6bde24523bebbd6
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-06-27 18:43:06 +0000

    updated with signed assembly
    git-svn-id: http://extf.googlecode.com/svn/trunk@641 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/SemWeb/Select.cs
+++ Xameleon/SemWeb/Select.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using SemWeb;
+﻿//using System;
+//using System.IO;
+//using System.Text;
+//using SemWeb;
 
-namespace Xameleon.SemWeb {
-    public class Select {
-        const string RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-        const string FOAF = "http://xmlns.com/foaf/0.1/";
+//namespace Xameleon.SemWeb {
+//    public class Select {
+//        const string RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+//        const string FOAF = "http://xmlns.com/foaf/0.1/";
 
-        static readonly Entity rdftype = RDF + "type";
-        static readonly Entity foafPerson = FOAF + "Person";
-        static readonly Entity foafknows = FOAF + "knows";
-        static readonly Entity foafname = FOAF + "name";
+//        static readonly Entity rdftype = RDF + "type";
+//        static readonly Entity foafPerson = FOAF + "Person";
+//        static readonly Entity foafknows = FOAF + "knows";
+//        static readonly Entity foafname = FOAF + "name";
 
-        public Select() { }
+//        public Select() { }
 
-        public static string Process(String foafURI) {
+//        public static string Process(String foafURI) {
 
-            StringBuilder builder = new StringBuilder();
-            Uri uri = new Uri(foafURI);
+//            StringBuilder builder = new StringBuilder();
+//            Uri uri = new Uri(foafURI);
 
-            Store store = new MemoryStore();
-            RdfReader reader = RdfReader.LoadFromUri(uri);
-            reader.BaseUri = uri.OriginalString;
-            store.Import(reader);
+//            Store store = new MemoryStore();
+//            RdfReader reader = RdfReader.LoadFromUri(uri);
+//            reader.BaseUri = uri.OriginalString;
+//            store.Import(reader);
 
-            builder.AppendLine("<statement>These are the people in the file: </statement>");
-            foreach (Statement s in store.Select(new Statement(null, rdftype, foafPerson))) {
-                foreach (Resource r in store.SelectObjects(s.Subject, foafname))
-                    builder.AppendLine("<entry>" + r + "</entry>");
-            }
+//            builder.AppendLine("<statement>These are the people in the file: </statement>");
+//            foreach (Statement s in store.Select(new Statement(null, rdftype, foafPerson))) {
+//                foreach (Resource r in store.SelectObjects(s.Subject, foafname))
+//                    builder.AppendLine("<entry>" + r + "</entry>");
+//            }
 
-            builder.AppendLine("<statement>And here's RDF/XML just for some of the file: </statement>");
+//            builder.AppendLine("<statement>And here's RDF/XML just for some of the file: </statement>");
 
-            using (RdfWriter w = new RdfXmlWriter(new StringWriter(builder))) {
-                store.Select(new Statement(null, foafname, null), w);
-                store.Select(new Statement(null, foafknows, null), w);
-            }
+//            using (RdfWriter w = new RdfXmlWriter(new StringWriter(builder))) {
+//                store.Select(new Statement(null, foafname, null), w);
+//                store.Select(new Statement(null, foafknows, null), w);
+//            }
 
-            return builder.ToString();
-        }
+//            return builder.ToString();
+//        }
 
-    }
+//    }
 
-}
+//}
