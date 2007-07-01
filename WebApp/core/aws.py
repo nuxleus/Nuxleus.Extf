047ae219dd9d55e8a263b0b8171b4ac5e681a0e4
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-01 16:08:24 +0000

    these are no longer necessary
    git-svn-id: http://extf.googlecode.com/svn/trunk@657 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- WebApp/core/aws.py
+++
@@ -1,7 +0,0 @@
-def lookup_keys():
-    f = file('awsenv.sh', 'rb')
-    pub_key = f.readline().strip()
-    secret_key = f.readline().strip()
-    #print pub_key, secret_key
-    return (pub_key, secret_key)
-    
