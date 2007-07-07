b179ef2d4108f60d5ede2fe1ded1e5531cf7d8ce
sylvain.hellegouarch <sylvain.hellegouarch@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-07 21:45:35 +0000

    Added some extra command line options
    git-svn-id: http://extf.googlecode.com/svn/trunk@683 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- WebApp/runcp.py
+++ WebApp/runcp.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import os.path
+import cherrypy
+from core.application import create_application
 
 def parse_commandline():
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option("-c", "--configuration", dest="configuration",
                       help="configuration file path")
+    parser.add_option("-p", "--port", dest="port",
+                      help="port to listen to", default="8080")
+    parser.add_option("-s", "--log-to-screen", dest="log_to_screen",
+                      help="log server to screen", action="store_true")
     parser.add_option("-a", "--aws", dest="aws",
                       help="path to the file containing the AWS keys")
     (options, args) = parser.parse_args()
@@ -14,18 +20,18 @@ def parse_commandline():
     return options
 
 def run_application():
-    from core.application import create_application
     cur_dir = os.path.dirname(os.path.abspath(__file__))
+    
     options = parse_commandline()
-    create_application(options, cur_dir)
-
-    import cherrypy
     cherrypy.config.update({'server.thread_pool': 15,
-                            'server.socket_port': 8080,
+                            'server.socket_port': int(options.port),
                             'engine.autoreload_on': False,
-                            'log.screen': True,
+                            'log.screen': options.log_to_screen,
                             'log.error_file': os.path.join(cur_dir, "error.log"),
                             'log.access_file': os.path.join(cur_dir, "access.log")})
+    
+    create_application(options, cur_dir)
+
     cherrypy.server.quickstart() 
     cherrypy.engine.start()
 
