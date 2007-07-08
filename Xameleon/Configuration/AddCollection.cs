dce419757c517b010207d7d8f716f8908693791e
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-08 22:50:32 +0000

    generic add elements
    git-svn-id: http://extf.googlecode.com/svn/trunk@701 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Configuration/AddCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace Xameleon.Configuration {
+
+  public class AddCollection : ConfigurationElementCollection {
+
+    public Add this[int index] {
+      get {
+        return base.BaseGet(index) as Add;
+      }
+      set {
+        if (base.BaseGet(index) != null) {
+          base.BaseRemoveAt(index);
+        }
+        this.BaseAdd(index, value);
+      }
+    }
+
+    protected override ConfigurationElement CreateNewElement() {
+      return new Add();
+    }
+
+    protected override object GetElementKey(ConfigurationElement element) {
+      return ((Add)element).Key;
+    }
+  }
+}
