c1df9f3a375f0795c416dc2d90bf6aced22961f7
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-08 19:44:54 +0000

    addition of several custom configuration handles
    git-svn-id: http://extf.googlecode.com/svn/trunk@697 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Configuration/AspNetXameleonTransform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+
+namespace Xameleon.Configuration {
+
+  public class AspNetXameleonConfiguration : ConfigurationSection {
+
+
+    public static AspNetXameleonConfiguration GetConfig() {
+      return ConfigurationManager.GetSection("xameleon") as AspNetXameleonConfiguration;
+    }
+
+    [ConfigurationProperty("useMemcached", DefaultValue = "no", IsRequired = false)]
+    public string UseMemcached {
+      get {
+        return this["useMemcached"] as string;
+      }
+    }
+
+    [ConfigurationProperty("defaultEngine", DefaultValue="Saxon", IsRequired = false)]
+    public string DefaultEngine {
+      get {
+        return this["defaultEngine"] as string;
+      }
+    }
+
+    [ConfigurationProperty("xsltParams")]
+    public XsltParamCollection XsltParams {
+      get {
+        return this["xsltParams"] as XsltParamCollection;
+      }
+    } 
+  }
+}
