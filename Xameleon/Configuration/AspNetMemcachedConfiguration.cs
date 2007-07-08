4767d50bff53866ea8422f4eb365b5369f66f6dd
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-08 20:20:26 +0000

    addition of memcached configuration section
    git-svn-id: http://extf.googlecode.com/svn/trunk@698 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Configuration/AspNetMemcachedConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+
+namespace Xameleon.Configuration {
+
+  public class AspNetMemcachedConfiguration : ConfigurationSection {
+
+
+    public static AspNetMemcachedConfiguration GetConfig() {
+      return ConfigurationManager.GetSection("aws") as AspNetMemcachedConfiguration;
+    }
+
+    [ConfigurationProperty("server", IsRequired = true)]
+    public MemcachedServerCollection AwsKeyCollection {
+      get {
+        return this["server"] as MemcachedServerCollection;
+      }
+    }
+
+    [ConfigurationProperty("poolConfig", IsRequired = true)]
+    public MemcachedPoolConfigCollection PoolConfig {
+      get {
+        return this["poolConfig"] as MemcachedPoolConfigCollection;
+      }
+    }
+  }
+}
---
+++ Xameleon/Configuration/MemcachedPoolConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace Xameleon.Configuration {
+
+  public class MemcachedPoolConfig : ConfigurationElement {
+
+    [ConfigurationProperty("property", IsRequired = true)]
+    public string Property {
+      get {
+        return this["key"] as string;
+      }
+    }
+
+    [ConfigurationProperty("value", IsRequired = true)]
+    public string Value {
+      get {
+        return this["value"] as string;
+      }
+    }
+  }
+}
