8aff791b58e03c35dc38edde036da512d2481b9b
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-11 12:03:52 +0000

    more tweaks
    git-svn-id: http://extf.googlecode.com/svn/trunk@742 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/HttpApplication/Global.cs
+++ Xameleon/HttpApplication/Global.cs
@@ -83,7 +83,7 @@ namespace Xameleon.HttpApplication {
           //HttpContext.Current.Response.Output.WriteLine("uri: " + (string)xslt.BaseUri);
           baseUri = (string)xslt.BaseUri;
         }
-        Uri uri = new Uri(baseUri, UriKind.Absolute);
+        Uri uri = new Uri("http://localhost/");
         _XsltCompiledHashtable.GetTransformer(xslt.Name, (string)xslt.Uri, uri, _Processor);
       }
 
