668f3c012f59aaaab41ab11266658e2d7a609ea2
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-07-19 07:57:37 +0000

    moved from Transform.Context
    git-svn-id: http://extf.googlecode.com/svn/trunk@833 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Cryptography/HashcodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Xameleon.Cryptography {
+
+  public enum HashAlgorithm { MD5, SHA1, SHA256 };
+
+  public class HashcodeGenerator {
+
+    static Encoding encoder = new UTF8Encoding();
+
+    public static String GetHMACMD5Base64String(string key, params object[] hashArray) {
+      StringBuilder builder = new StringBuilder();
+      HMACMD5 hmacMD5 = new HMACMD5(encoder.GetBytes(key));
+      foreach (object obj in hashArray) {
+        builder.Append(obj);
+      }
+      return Convert.ToBase64String(hmacMD5.ComputeHash(encoder.GetBytes(builder.ToString())));
+    }
+    public static String GetHMACSHA1Base64String(string key, params object[] hashArray) {
+      StringBuilder builder = new StringBuilder();
+      HMACSHA1 hmacMD5 = new HMACSHA1(encoder.GetBytes(key));
+      foreach (object obj in hashArray) {
+        builder.Append(obj);
+      }
+      return Convert.ToBase64String(hmacMD5.ComputeHash(encoder.GetBytes(builder.ToString())));
+    }
+    public static String GetHMACSHA256Base64String(string key, params object[] hashArray) {
+      StringBuilder builder = new StringBuilder();
+      HMACSHA256 hmacMD5 = new HMACSHA256(encoder.GetBytes(key));
+      foreach (object obj in hashArray) {
+        builder.Append(obj);
+      }
+      return Convert.ToBase64String(hmacMD5.ComputeHash(encoder.GetBytes(builder.ToString())));
+    }
+  }
+}
