5af5c6956c5556b9fd07929192ecd841b6bc728d
xmlhacker <xmlhacker@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-09-17 12:53:17 +0000

    addition of core nuXleus library
    git-svn-id: http://extf.googlecode.com/svn/branches/development-0.2.3@974 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Nuxleus/FileSystem/Watcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Permissions;
+
+namespace Nuxleus.FileSystem
+{
+    public class Watcher : FileSystemWatcher
+    {
+        string _path;
+        TextWriter _logWriter;
+        NotifyFilters _notifyFilters;
+        string _filter;
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public Watcher(string path, string filter, TextWriter logWriter)
+        {
+            _path = path;
+            _logWriter = logWriter;
+            _filter = filter;
+            _notifyFilters =
+                NotifyFilters.LastAccess    |
+                NotifyFilters.LastWrite     |
+                NotifyFilters.FileName      |
+                NotifyFilters.DirectoryName |
+                NotifyFilters.Size          |
+                NotifyFilters.CreationTime  |
+                NotifyFilters.Attributes;
+        }
+
+        public TextWriter LogWriter { get { return _logWriter; } set { _logWriter = value; } }
+        public string Folder { get { return _path; } set { _path = value; } }
+        public NotifyFilters NotifyFilters { get { return _notifyFilters; } set { _notifyFilters = value; } }
+        public string FileFilter { get { return _filter; } set { _filter = value; } }
+
+        public void Watch(bool watchSubDirectories)
+        {
+            base.Path = _path;
+            base.NotifyFilter = _notifyFilters;
+
+            base.IncludeSubdirectories = watchSubDirectories;
+            base.Filter = _filter;
+
+            this.Changed += new FileSystemEventHandler(OnChanged);
+            this.Created += new FileSystemEventHandler(OnChanged);
+            this.Deleted += new FileSystemEventHandler(OnChanged);
+            this.Renamed += new RenamedEventHandler(OnRenamed);
+
+            this.EnableRaisingEvents = true;
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Watcher watcher = (Watcher)source;
+            watcher.LogWriter.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            Watcher watcher = (Watcher)source;
+            watcher.LogWriter.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
