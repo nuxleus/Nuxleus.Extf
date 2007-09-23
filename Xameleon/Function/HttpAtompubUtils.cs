7b240aa43a0949f6664d3fa3f301816f52317818
sylvain.hellegouarch <sylvain.hellegouarch@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-09-23 02:03:22 +0000

    First attempt for an atom service
    git-svn-id: http://extf.googlecode.com/svn/branches/development-0.2.3@1009 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

---
+++ Xameleon/Function/HttpAtompubUtils.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Xml;
+
+namespace Xameleon.Function {
+  public class HttpAtompubUtils {
+
+      public HttpAtompubUtils() { }
+
+    public static String GenerateEntry(HttpContext context) {
+      return "hi";
+    }
+  }
+}
