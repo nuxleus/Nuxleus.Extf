1a5f8ebe7d2b635c0fbef2d6891b7d9da8900d50
sylvain.hellegouarch <sylvain.hellegouarch@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-09-23 03:55:16 +0000

    added notification call
    git-svn-id: http://extf.googlecode.com/svn/branches/development-0.2.3@1020 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Function/HttpAtompubUtils.cs
+++ Xameleon/Function/HttpAtompubUtils.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Web;
 using System.Xml;
-using net.sf.saxon.value;
-using Saxon.Api;
+using Xameleon.Llup;
 
 namespace Xameleon.Function {
   public class HttpAtompubUtils {
@@ -10,7 +9,11 @@ namespace Xameleon.Function {
       public HttpAtompubUtils() { }
 
     public static string GenerateEntry(HttpContext context) {
-      return "<b>hey there</b>";
+
+      string s = "<qs xmlns='http://purl.oclc.org/DEFUZE/qs' resp='1e582a22b3f82a7c9acc227b0ccb6fdec0d84e6b74dccbcb542e8d11e1b379dd' type='response'><op><push-message /></op><qid>test</qid><mid>3664f5666c2564f7fb6c16d45b1e5f7f951ebff6d7d34792fe4badb63b566ed2</mid><payload>PGxsdXA6bm90aWZpY2F0aW9uIHhtbG5zOmxsdXA9Imh0dHA6Ly93d3cueDJ4Mngub3JnL2xsdXAiIGFjdGlvbj0iIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwNS9BdG9tIj48bGx1cDpyZWNpcGllbnQgaHJlZj0iIiAvPjxsaW5rIGhyZWY9Imh0dHA6Ly9hdG9tcHViLmRlZnV6ZS5vcmcvc3RhcnRlci9zYWxhZC9OYXBvbGl0YW4uYXRvbSIgcmVsPSJzZWxmIiB0eXBlPSJhcHBsaWNhdGlvbi9hdG9tK3htbDt0eXBlPWVudHJ5IiAvPjxjYXRlZ29yeSB0ZXJtPSJpbmRpZSIgLz48Y2F0ZWdvcnkgdGVybT0icm9jayIgLz48Y2F0ZWdvcnkgdGVybT0iamF6eiIgLz48Y2F0ZWdvcnkgdGVybT0iYmx1ZXMiIC8+PC9sbHVwOm5vdGlmaWNhdGlvbj4=</payload></qs>";
+
+      Notification n = Notification.Parse(s);
+      return "";
     }
   }
 }
