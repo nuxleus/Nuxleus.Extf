0417127388cfe493b006f85feca93232f28aa183
sylvain.hellegouarch <sylvain.hellegouarch@ebf17cb3-5128-0410-bc1c-09dfd2e4f19d>
2007-09-23 02:15:34 +0000

    To XdmNode
    git-svn-id: http://extf.googlecode.com/svn/branches/development-0.2.3@1013 ebf17cb3-5128-0410-bc1c-09dfd2e4f19d

--- Xameleon/Function/HttpAtompubUtils.cs
+++ Xameleon/Function/HttpAtompubUtils.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Web;
 using System.Xml;
+using net.sf.saxon.value;
+using Saxon.Api;
 
 namespace Xameleon.Function {
   public class HttpAtompubUtils {
 
       public HttpAtompubUtils() { }
 
-    public static XmlNode GenerateEntry(HttpContext context) {
+    public static XdmNode GenerateEntry(HttpContext context) {
       XmlDocument doc = new XmlDocument();
       doc.Load("<b>hey there</b>");
       
-      return doc.DocumentElement;
+      Processor processor = new Processor();
+      return processor.NewDocumentBuilder().Build(doc.DocumentElement);
     }
   }
 }
